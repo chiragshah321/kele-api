0904ac1700453f1062bcdb2445576526174bdebb
Chirag Shah <chiragshah321@gmail.com>
2017-06-19 23:19:42 +0000

    checkpoint5 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,16 @@
 require 'httparty'
 require 'json'
+require './lib/roadmap'
 
 
 class Kele
     include HTTParty
+    include MyRoadMap
 
   def initialize(email, password)
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password })
-    raise 'Invalid Email and/or Password' if response.code != 200
-    @auth_token = response["auth_token"]
+      response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { "email": email, "password": password })
+      raise 'Invalid Email and/or Password' if response.code != 200
+      @auth_token = response["auth_token"]
   end
   
   def get_me 
@@ -20,7 +22,6 @@ class Kele
       response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
       @mentor_availability = JSON.parse(response.body)
   end
-  
 end
 
 
